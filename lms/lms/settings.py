6a9bc9c95649a778858a2463f4ec7e4b25b88894
Shafin98 <98sasiam@gmail.com>
2026-05-04 13:20:19 +0600

    credentials will be loaded from .env

--- lms/lms/settings.py
+++ lms/lms/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.getenv('SECRET_KEY')
-SECRET_KEY = 'django-insecure-u!931o(!%5d9zml63t@9-ljrsdu0nohi@(x2l1)ddj&p%0y76y'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -166,9 +165,6 @@ EMAIL_HOST = 'smtp.office365.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-# DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
-EMAIL_HOST_USER = 'no-reply@plat-forms.com'
-EMAIL_HOST_PASSWORD = 'sncmgcmzjkzgxlqn'
-DEFAULT_FROM_EMAIL = 'Plat-Forms <no-reply@plat-forms.com>'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
