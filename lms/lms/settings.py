f739b66849ccdd16e52465d8d275f492a14f7fa7
Shafin98 <98sasiam@gmail.com>
2026-05-04 16:46:05 +0600

    email changed to gmail

--- lms/lms/settings.py
+++ lms/lms/settings.py
@@ -172,7 +172,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # and add the credentials to your .env file.
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.office365.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
